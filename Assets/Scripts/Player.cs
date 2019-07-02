c3af96ab63b1733275792956e45061bf8a2ff9d4
Szymon Balasz <sbalasz@gmail.com>
2019-07-02 22:50:17 +0200

    implemented horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    //cached references
+    Rigidbody2D myRigidBody;
+
+    [Header("Movement")]
+    [SerializeField] float playerMoveSpeed = 5f;
+
+    void Start()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        float controlThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        Vector2 playerVelocity = new Vector2(controlThrow * playerMoveSpeed, myRigidBody.velocity.y);
+        myRigidBody.velocity = playerVelocity;
+    }
+}
