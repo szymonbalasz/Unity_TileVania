6e6637f5f846fa2300b624d64913c5421b0d4908
Szymon Balasz <sbalasz@gmail.com>
2019-07-05 19:42:38 +0200

    added check to fire nextlevel trigger only once

--- Assets/Scripts/LevelExit.cs
+++ Assets/Scripts/LevelExit.cs
@@ -6,12 +6,12 @@ using UnityEngine.UI;
 
 public class LevelExit : MonoBehaviour
 {
-
+    bool triggered = false; // to prevent players' extra collider from triggering level skips
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.name != "Player") { return; }
+        if (triggered) { return; }
+        triggered = true;
         FindObjectOfType<GameSession>().LevelComplete();
     }
-
-
 }
